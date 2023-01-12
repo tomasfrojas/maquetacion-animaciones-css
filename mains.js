12e5af61e9d5d2858cc9ea2380826060c0f3e138
tompirojas <rojas.tomasfelipe@gmail.com>
2023-01-12 18:00:44 -0500

    Curso de animaciones con CSS

---
+++ mains.js
@@ -0,0 +1,13 @@
+const button1 = document.querySelector("#button1");
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal__content--close");
+
+button1.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+});
+
+closeButton.addEventListener("click", () => {
+  modal.classList.add("hidden");
+  modal.classList.remove("visible");
+});
